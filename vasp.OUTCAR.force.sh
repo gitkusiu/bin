217faee07dd3fdbf44cbcdadc0b1f6e93826642d
Krzysztof Kosmider <kkusiu@gmail.com>
2014-02-18 15:14:17 +0100

    Improving vasp.OUTCAR.force.sh scirpt

--- vasp.OUTCAR.force.sh
+++ vasp.OUTCAR.force.sh
@@ -4,27 +4,25 @@ outcar=$3
 tip_begin=$1
 tip_end=$2
 
-#echo $outcar 
-number_of_steps=`grep POSITION $outcar | wc | awk '{print $1}'`
+buffor=100
 
+natoms=`grep -m 1 NIONS $outcar | awk '{print $12}'`
 
-awk -v nsteps=$number_of_steps -v tip_begin=$tip_begin -v tip_end=$tip_end 'BEGIN {    
-    getline;
+linetot=`wc $outcar|awk '{print $1}'`
+line_from_the_end=`tac $outcar | grep -n POSITION  -m 1  | awk '{print $1}'| sed -e 's/:.*//g'`
+no_lices_to_pars=`echo "$line_from_the_end + $buffor" | bc`
+
+tail -n $no_lices_to_pars $outcar | awk -v natoms=$natoms -v tip_begin=$tip_begin -v tip_end=$tip_end 'BEGIN {    
     is_found = 0;
-    n_atoms
-        while(is_found < nsteps)
+    while(is_found == 0)
     {
-
-        if($10 == "NIONS")
-        {
-            n_atoms = $12
-        }
+        getline;
         if($1 == "POSITION")
         {
-            is_found = is_found+1;
+            is_found = 1;
         }
-        getline;
     }
+    getline
     i=1
     force_sum[1] = 0.0
     force_sum[2] = 0.0
@@ -32,33 +30,16 @@ awk -v nsteps=$number_of_steps -v tip_begin=$tip_begin -v tip_end=$tip_end 'BEGI
 }
 
 {
-
-#    print i, tip_begin, tip_end 
-    if( i>=tip_begin && i<=tip_end && i <= n_atoms)
+    if( i>=tip_begin && i<=tip_end && i <= natoms)
     {
-#        forces[i,1] = $4
-#        forces[i,2] = $5
-#        forces[i,3] = $6
         force_sum[1] = force_sum[1] + $4
         force_sum[2] = force_sum[2] + $5
-        force_sum[3] = force_sum[3] + $6
+        force_sum[3] = force_sum[3] + $6    
     }
     i = i+1
 }
 
 END {
-#    for(i=tip_begin; i<=tip_end; i++)
-#    {
-#        printf "%e   %e   %e\n", forces[i,1], forces[i,2], forces[i,3];
-#    }
-#    print "ssss"
-
     printf "%e   %e   %e \n", force_sum[1], force_sum[2], force_sum[3]; 
 
-}' $outcar
-
-
-#awk -v nsteps=$number_of_steps -f $HOME/bin/dft/vasp.OUTCAR.last.forces.awk $1
-
-
-
+}'
