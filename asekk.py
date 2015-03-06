b4d37cdfc8ed330fb668d39951d0ecf39a349c89
Krzysztof Kosmider <kkusiu@gmail.com>
2015-03-06 18:22:55 +0100

    adding asekk.py rotate.py files

---
+++ asekk.py
@@ -0,0 +1,58 @@
+import numpy as np
+from math import cos, sin, pi
+
+#####################################
+#
+# rotate - rotate 'vector' around 'axis'
+#          by angle 'alpha' in respect to 'origin' 
+#
+#######################################
+def rotate(vector, alpha, axis='z', origin=[0.0,0.0,0.0]):
+    a=(alpha*pi)/180.0
+    o=np.array(origin)
+    v=np.array(vector)
+    v1=np.add(v,-o)
+    if(axis=='z'):
+        matrix = [[ cos(a),-sin(a), 0.0   ],
+                  [ sin(a), cos(a), 0.0   ],
+                  [ 0.0   , 0.0   , 1.0   ]]
+    elif(axis=='x'):
+        matrix = [[ 1.0   , 0.0   , 0.0   ],
+                  [ 0.0   , cos(a),-sin(a)],
+                  [ 0.0   , sin(a), cos(a)]]
+    elif(axis=='y'):
+        matrix = [[ cos(a), 0.0   , sin(a)],
+                  [ 0.0   , 1.0   , 0.0   ],
+                  [-sin(a), 0.0   , cos(a)]]
+    m = np.array(matrix)
+    v2=np.dot(m,v1)
+    return np.add(o,v2)
+
+
+#####################################
+#
+# rotate secected atoms
+#
+#######################################
+def rotate_atoms(atoms, alpha, fromto=[-1,-1], axis='z', origin=[0.0,0.0,0.0]):
+    p=atoms.arrays['positions']
+    n=len(p)
+    ifrom, ito  = 0,0
+    if(fromto[0]==-1 and fromto[1]==-1):
+        ifrom  = 1
+        ito    = len(atoms)
+    else:
+        ifrom  = fromto[0]
+        ito    = fromto[1]
+    for i in range(ifrom-1,ito):
+        p[i] = rotate(p[i],alpha,axis=axis,origin=origin)
+#        p[i] = asekk.rotate(p[i],alpha,axis=axis,origin=origin)
+
+
+
+
+
+
+
+
+
---
+++ rotate.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import asekk
+import sys
+import numpy as np
+from math import cos, sin, pi
+
+from ase.io.vasp import read_vasp
+from ase.io.vasp import write_vasp
+
+
+num = len(sys.argv)
+poscar = read_vasp(sys.argv[num-1])
+
+p=poscar.arrays['positions']
+n=len(p)
+#for i in range(n):
+#    print p[i]
+
+kat=-45.0
+
+#for i in range(n):
+#    p[i] = asekk.rotate(p[i],kat,'y',origin=p[0])
+
+asekk.rotate_atoms(poscar, kat, fromto=[2,2], axis='z', origin=p[0])
+
+write_vasp(sys.stdout,poscar,label="moj test", direct=False,sort=False,vasp5=True)
+
+#v1=[2.0,0.0,0.0]
+#v2=rotate(vector=v1,alpha=-90.0,axis='z',origin=[1.0,0.0,0.0])
+#print v1
+#print v2
