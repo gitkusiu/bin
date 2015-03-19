a69e595650151dad9e31cf02ed60ea067fec15ac
Krzysztof Kosmider <kkusiu@gmail.com>
2015-03-19 12:54:49 +0100

    improvements of aims.eigen.py scripts

--- aims.eigen.py
+++ aims.eigen.py
@@ -8,7 +8,13 @@
 #
 ######################################
 import sys
-import numpy as np 
+import numpy as np
+
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("--fermi_in_zero",  action="store_true", default=False, help="If eigenvalues should be with Ferme lavel in the orgin")
+(options, args) = parser.parse_args()
 
 num = len(sys.argv)
 f  = open(sys.argv[num-1], "r")
@@ -16,7 +22,7 @@ lines = f.read().splitlines()
 
 # read: n - number od KS eigenvalues, index of laste eigenvalues bloc
 n, index = 0, 0
-ferm = 0.0
+fermi = 0.0
 for i, line in enumerate(lines):
     if "Number of Kohn-Sham states (occupied + empty)" in line:
         l = line.split()
@@ -25,7 +31,7 @@ for i, line in enumerate(lines):
         index=i+1
     if "Chemical potential (Fermi level) in eV" in line:
         l    = line.split()
-        ferm = float(l[8])
+        fermi = float(l[8])
     
 
 if(index==0 and index < 0):
@@ -35,11 +41,11 @@ else:
     eigens = []
     for i in range(index,index+n):
         l=lines[i].split()
-        eigens.append(float(l[3])-ferm)
+        if(options.fermi_in_zero):
+            eigens.append(float(l[3])-fermi)
+        else:
+            eigens.append(float(l[3]))
 
 print 0.0, ' '.join(map(str, eigens))
 print 1.0, ' '.join(map(str, eigens))
-#print 1.0, en
-##perint eigenvalues
-#for en in eigens:
-#    print en
+
