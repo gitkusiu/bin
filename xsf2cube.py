c1920f2d911f1cec0e32beb33dfbe1d7d8c19452
Krzysztof Kosmider <kkusiu@gmail.com>
2015-04-16 16:06:35 +0200

    add xsf2cube.py

---
+++ xsf2cube.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+from ase.io.xsf import read_xsf
+from ase.io.cube import write_cube
+#from ase        import Atoms
+
+num = len(sys.argv)
+xsf = read_xsf(sys.argv[num-1],read_data=True)
+
+write_cube(sys.stdout,xsf[1],xsf[0])
+#write_cube("tmp.cube",xsf[1],xsf[0])
+
