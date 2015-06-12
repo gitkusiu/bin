0ab730378a5b94cb0155806660ed359a01cc1480
Krzysztof Kosmider <kkusiu@gmail.com>
2015-06-12 10:02:24 +0200

    developement of the ase.surface.py script

--- ase.surface.py
+++ ase.surface.py
@@ -9,6 +9,7 @@ from ase.io.aims import write_aims
 #from ase.io.xyz  import write_xyz
 
 from ase.lattice.surface import fcc111
+from ase.lattice.surface import fcc110
 import math
 from optparse import OptionParser
 
@@ -17,6 +18,11 @@ parser.add_option("-a", "--lattice",    action="store", type="float", default=1.
 parser.add_option("-p", "--periods",    action="store", type="int",    default=[1,1,1],       help="repetition of the unit cell", nargs=3)
 parser.add_option("-v", "--vaccuum",    action="store", type="float", default=1.0)
 parser.add_option("-e", "--element",    action="store", type="string", default='H')
+parser.add_option("-o", "--ortogonal",  action="store_true", default=False)
+parser.add_option("-b", "--bravais",    action="store", type="string", default='fcc')
+parser.add_option(      "--hkl",        action="store", type="string", default='111')
+
+
 (options, args) = parser.parse_args()
 
 
@@ -31,8 +37,13 @@ p=options.periods
 v=options.vaccuum
 e=options.element
 
-atoms = fcc111(e, size=p,a=l, vacuum=v, orthogonal=False)
-#atoms = fcc111(e, size=p,a=l, vacuum=v)
+atoms = []
+if(options.bravais == 'fcc'):
+    if(options.hkl == '111'):
+        #atoms = fcc111(e, size=p,a=l, vacuum=v, orthogonal=False)
+        atoms = fcc111(e, size=p,a=l, vacuum=v, orthogonal=options.ortogonal)
+    elif(options.hkl == '110'):
+        atoms = fcc110(e, size=p,a=l, vacuum=v)
 
 #c=atoms.get_cell()
 #c[1][0] += l*math.sqrt(2)*0.5
@@ -40,6 +51,7 @@ atoms = fcc111(e, size=p,a=l, vacuum=v, orthogonal=False)
 
 #atoms.translate([0.0,0.0,-14.9999999999999982])
 
+print atoms
 tmpname="geometry.in.tmp"
 write_aims(tmpname, atoms)
 f = open(tmpname, 'r')
